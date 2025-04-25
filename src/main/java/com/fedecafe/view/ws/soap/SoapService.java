187428ce9f232c632690687884a73d998ef49eed
hyela <henry.yela@gmail.com>
2025-04-25 14:45:47 -0500

    Update SoapService.java

--- src/main/java/com/fedecafe/view/ws/soap/SoapService.java
+++ src/main/java/com/fedecafe/view/ws/soap/SoapService.java
@@ -4,7 +4,10 @@ import jakarta.jws.WebService;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
 
-@WebService(targetNamespace = "http://cafedecolombia.com/wsdl")
+@WebService(
+	    name = "SoapService",
+	    targetNamespace = "http://cafedecolombia.com/wsdl"
+	)
 public interface SoapService {
     @WebMethod
     String saludar(@WebParam(name = "nombre") String nombre);
